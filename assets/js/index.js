0bda9a1c1338554599eea9fc9cd18de513480211
nandemotoken <nandemotoken@gmail.com>
2021-03-31 02:55:58 +0000

    update testnets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,22 @@
 
+//--------------general settings--------------
+
 //contractAddress
-const Address = "0x427444759BB5D100CC7483E9E32bdB32DE577b06";
+const ContractAddress = "0x09F06a6964A1bcee82BE6C2B00B1f7eDb4D3DcC4";
+
+//matic matic testnet(0) or matic network(1)
+const network = 0;
+
+//---------------------------------------------
+
+
+//testnet or mainnet 
+//rpclist = [ "" , "" ];
+explorerlist = [ "https://explorer-mumbai.maticvigil.com/tokens/" , "https://explorer-mainnet.maticvigil.com/tokens/" ];
+mintnandemotokenapilist = [ "https://mint.nandemotoken.com/api/v1/testnet/" , "https://mint.nandemotoken.com/api/v1/" ];
+opensealist = [ "https://testnets.opensea.io/account/" , "https://opensea.io/account/"];
+
+
 let replica_contract;
 
 window.onload = async function() {
@@ -10,43 +26,6 @@ window.onload = async function() {
 function walletmodal(){
     $('#wallet-popup').modal('show');
 }
-
-
-async function loadmm_matic(){
-    $('#wallet-popup').modal('hide');
-    await window.ethereum.enable();
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const add = await signer.getAddress();
-    console.log(add);
-    const chainId = await ethereum.request({ method: 'eth_chainId' });
-    console.log(chainId);
-    if (chainId == "0x89" ){
-        console.log("MaticNetworkに正しく接続しています")
-    } else {
-        ans = window.confirm("MetaMaskのネットワークをMaticに切り替えて下さい\n参考：https://note.com/ocurima/n/nd3191fdd7acd\n\n参考サイトを開きますか？");
-        if(ans){ window.open("https://note.com/ocurima/n/nd3191fdd7acd"); }
-        return;
-    }
-    replica_contract = await new ethers.Contract( Address , abi , signer );
-//    window.alert(replica_contract.address);
-    const maticbalance = await signer.getBalance();
-//    window.alert(maticbalance);
-    if ( maticbalance == 0 ){
-        window.alert("maticを入手してください");
-    }　else {
-        meta = await replica_contract.mint();
-//        window.alert(meta);
-        window.alert("トランザクションを送信しています。20秒程度お待ちください");
-        filter = replica_contract.filters.Transfer(null, add);
-        replica_contract.on(filter, (from, to, amount, event) => {
-        ans = window.confirm("OpenSeaでNFTレプリカを確認しましょう\n参考：https://opensea.io/account\n\nOpenSeaを開き、MetaMaskを接続しますか？\n(画像の反映などには数分時間がかかることがあります)");
-        if(ans){ window.open("https://opensea.io/account"); }
-
-    });
-    }
-}
-
 
 
 async function loadmm_gasfree(){
@@ -64,13 +43,21 @@ async function loadmm_gasfree(){
         return;
     }
     
+    
     await window.ethereum.enable();
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const add = await signer.getAddress();
 
-    // let result = await $.getJSON( "https://mint.nandemotoken.com/api/v1/0x427444759BB5D100CC7483E9E32bdB32DE577b06/"+ add + "/" )
-    // console.log(result);
+    ans2 = await signer.signMessage( "NFTレプリカを受け取ります" );
+    if ( ans2[1] != "x" ){
+        return;
+    }
+    
+    
+    
+    let result = await $.getJSON( mintnandemotokenapilist[network] + ContractAddress + "/"+ add + "/" )
+    console.log(result);
     $('#myinfo').modal('show')
     
     //    window.alert("開発中です。しばらくお待ちください");
@@ -78,13 +65,12 @@ async function loadmm_gasfree(){
 
 function explorer(){
     //window.alert("matic")
-    ans = window.confirm("ブロックチェーンエクスプローラーを開く\n\nhttps://explorer-mainnet.maticvigil.com/tokens/" +Address + "/inventory\n\nNFTの発行状況を確認しますか？(通常は1分以内に発行されます)");
-        if(ans){ window.open("https://explorer-mainnet.maticvigil.com/tokens/" +Address + "/inventory"); }
-
+    ans = window.confirm("ブロックチェーンエクスプローラーを開く\n\n" + explorerlist[network] + ContractAddress + "/inventory\n\nNFTの発行状況を確認しますか？(通常は1分以内に発行されます)");
+        if(ans){ window.open( explorerlist[network] +ContractAddress + "/inventory"); }
 }
 
 function opensea(){
     //window.alert("opensea")
-    ans = window.confirm("OpenSeaでNFTレプリカを確認する\n\nhttps://opensea.io/account\n\nOpenSeaを開き、MetaMaskを接続しますか？\n(反映には数分時間がかかります。NFTの画像は処理が終わると表示されます)");
-        if(ans){ window.open("https://opensea.io/account"); }
+    ans = window.confirm("OpenSeaでNFTレプリカを確認する\n\n"+ opensealist[network] + "\n\nOpenSeaを開き、MetaMaskを接続しますか？\n(反映には数分時間がかかります。NFTの画像は処理が終わると表示されます)");
+        if(ans){ window.open( opensealist[network] ); }
 }
